5640929ea1a6cbfe8ea1d8e7eca596b48e113c43
DangerRanger7 <kierradachelle@yahoo.com>
2018-02-24 22:37:37 -0600

    able to textview for AboutActivity

---
+++ app/src/main/java/kurtandkierra/simon/AboutActivity.java
@@ -0,0 +1,17 @@
+package kurtandkierra.simon;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Kierra on 2/22/2018.
+ */
+
+public class AboutActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
