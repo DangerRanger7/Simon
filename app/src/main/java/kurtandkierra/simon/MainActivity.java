ef8e11874584440120c5bfc5545bc4e44114f0ad
DangerRanger7 <kierradachelle@yahoo.com>
2018-02-25 08:46:57 -0600

    Added the PlayActivity

--- app/src/main/java/kurtandkierra/simon/MainActivity.java
+++ app/src/main/java/kurtandkierra/simon/MainActivity.java
@@ -1,7 +1,11 @@
 package kurtandkierra.simon;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +13,32 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //create button for AboutActivity
+        Button b = findViewById(R.id.about_button);
+
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),
+                        AboutActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+        //play button
+        findViewById(R.id.play_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),
+                        PlayActivity.class);
+                startActivity(intent);
+
+            }
+
+        });
     }
-}
+
+    }
+
