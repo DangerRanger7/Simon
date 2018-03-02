b1f8f980d36fb1793cbe0b4f883ebda3280eeea6
DangerRanger7 <kierradachelle@yahoo.com>
2018-03-01 20:46:44 -0600

    sounds onClick

--- app/src/main/java/kurtandkierra/simon/PlayActivity.java
+++ app/src/main/java/kurtandkierra/simon/PlayActivity.java
@@ -1,13 +1,18 @@
 package kurtandkierra.simon;
 
 import android.app.Activity;
+import android.media.AudioAttributes;
+import android.media.SoundPool;
 import android.os.Bundle;
 import android.text.style.BackgroundColorSpan;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
+import java.util.HashSet;
 import java.util.Random;
+import java.util.Set;
 import java.util.Timer;
 
 /**
@@ -16,12 +21,19 @@ import java.util.Timer;
 
 public class PlayActivity extends Activity {
     final Button buttonColor[] = new Button[4];
+    //soundpool
+    private SoundPool soundPool;
+    private Set<Integer> soundsLoaded;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
 
        //final int buttonColor[] = new int[4];
+
+        //for soundsLoaded
+        soundsLoaded = new HashSet<Integer>();
 
         //button id
         buttonColor[0] = findViewById(R.id.green_button);
@@ -43,7 +55,72 @@ public class PlayActivity extends Activity {
         sequence();
 
     }
-        //sequence for buttons*******************************************************************************************
+    //onResume
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        AudioAttributes.Builder attrBuilder = new AudioAttributes.Builder();
+        attrBuilder.setUsage(AudioAttributes.USAGE_GAME);
+
+        SoundPool.Builder spBuilder = new SoundPool.Builder();
+        spBuilder.setAudioAttributes(attrBuilder.build());
+        spBuilder.setMaxStreams(2);
+        soundPool = spBuilder.build();
+
+        soundPool.setOnLoadCompleteListener(new SoundPool.OnLoadCompleteListener() {
+            @Override
+            public void onLoadComplete(SoundPool soundPool, int sampleId, int status) {
+                if (status == 0){//means it was a success
+                    soundsLoaded.add(sampleId);
+                }else{
+                    Log.i("Sound","ERROR CAN'T LOAD SOUND");
+                }
+            }
+        });
+
+        //load sounds
+        final int gameBeepId = soundPool.load(this, R.raw.gamebeep, 1);
+        //green_button
+        findViewById(R.id.green_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (soundsLoaded.contains(gameBeepId)){
+                    soundPool.play(gameBeepId, 1.0f, 1.0f, 0,0,1.0f);
+                }
+               
+            }
+        });
+        //red_button
+        findViewById(R.id.red_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (soundsLoaded.contains(gameBeepId)){
+                    soundPool.play(gameBeepId, 1.0f, 1.0f, 0,0,1.0f);
+                }
+            }
+        });
+        //yellow_button
+        findViewById(R.id.yellow_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (soundsLoaded.contains(gameBeepId)){
+                    soundPool.play(gameBeepId, 1.0f, 1.0f, 0,0,1.0f);
+                }
+            }
+        });
+        //blue_button
+        findViewById(R.id.blue_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (soundsLoaded.contains(gameBeepId)){
+                    soundPool.play(gameBeepId, 1.0f, 1.0f, 0,0,1.0f);
+                }
+            }
+        });
+    }
+
+    //sequence for buttons*******************************************************************************************
         public void sequence(){
            // int x = 0;\
             int num = 0;
